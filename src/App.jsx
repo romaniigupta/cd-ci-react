e7f941d390d9d3ea5b3c13beeeb165d9d609a21f
romanigupta <romani.guptaa@gmail.com>
2025-05-02 16:56:28 +0530

    Added full project files

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return(
+    <div>
+    <h1>My React App</h1>
+    <h2>my react Implementaion with Ci/Cd</h2>
+    <h2>with github Action</h2>
+    </div>
+
+  )
+
+}
+export default App
